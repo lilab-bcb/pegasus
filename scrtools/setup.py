5d2cc0d2821a4167ed2b636ad4bc66fccb60f387
Bo Li <libo@broadinstitute.org>
2018-03-28 21:49:14 -0400

    Updated setup.py

--- scrtools/setup.py
+++ scrtools/setup.py
@@ -4,136 +4,56 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+	long_description = f.read()
+
+requires = ['scanpy>=0.4',
+			'numpy',
+			'pandas',
+			'scipy',
+			'natsort',
+			'seaborn',
+			'json',
+			'tables',
+			'sklearn',
+			'xlsxwriter',
+			'matplotlib',
+			'fisher',
+			'statsmodels'
+		  ]
 
 setup(
-    name='scrtools',
-    version='0.1.0',
-    description='scRNA-Seq analysis tools that scale to millions of cells, built upon scanpy',
-    long_description = long_description,
-
-    # This should be a valid link to your project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/pypa/sampleproject',  # Optional
-
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='The Python Packaging Authority',  # Optional
-
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='pypa-dev@googlegroups.com',  # Optional
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
-    keywords='sample setuptools development',  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],  # Optional
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-    package_data={  # Optional
-        'sample': ['package_data.dat'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-        'Funding': 'https://donate.pypi.org',
-        'Say Thanks!': 'http://saythanks.io/to/example',
-        'Source': 'https://github.com/pypa/sampleproject/',
-    },
+	name='scrtools',
+	version='0.1.0',
+	description='scRNA-Seq analysis tools that scale to millions of cells, built upon scanpy',
+	long_description = long_description,
+	url='https://github.com/broadinstitute/scRNA-Seq/tree/master/scrtools',
+	author='Bo Li, Joshua Gould',
+	author_email='libo@broadinstitute.org',
+	classifiers=[ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Framework :: Jupyter',
+		'Natural Language :: English',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: POSIX :: Linux',
+		'Topic :: Scientific/Engineering :: Bio-Informatics'
+	],
+	keywords='single cell RNA-Seq data analysis', 
+	packages=find_packages(),
+	install_requires=requires,
+	python_requires='~=3.5',
+	package_data={
+		'annotate_cluster': ['cell_type_markers.json'],
+	},
+	entry_points={
+		'console_scripts': [
+			'scrtools=scrtools.__main__:main',
+		],
+	},
 )
