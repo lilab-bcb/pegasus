24c664bdb0df3a41d330b08b39bdf33942aac425
Bo Li <libo@broadinstitute.org>
2018-05-03 17:54:09 -0400

    Added diffusion_map fast implementation

--- scrtools/scrtools/clustering/clustering.py
+++ scrtools/scrtools/clustering/clustering.py
@@ -1,14 +1,31 @@
 import os
+import time
 import numpy as np
 import pandas as pd
-import scanpy.api as sc
+import anndata
+import tables
 import xlsxwriter
+from scipy.sparse import csr_matrix
 from collections import Counter
 
-from . import batch_correction
+from . import batch_correction, diffusion_map
 
 
 transfer_gene_name = [(358, 'ENSG00000268991', 'FAM231C.2'), (921, 'ENSG00000278139', 'AL358075.4'), (2207, 'ENSG00000232995', 'RGS5.2'), (5847, 'ENSG00000282827', 'AC134772.2'), (5938, 'ENSG00000271858', 'CYB561D2.2'), (6087, 'ENSG00000241572', 'PRICKLE2-AS1.2'), (7213, 'ENSG00000249428', 'CFAP99.2'), (9596, 'ENSG00000280987', 'MATR3.2'), (9605, 'ENSG00000279686', 'AC142391.1'), (10277, 'ENSG00000282913', 'BLOC1S5.2'), (10867, 'ENSG00000124593', 'AL365205.1'), (11619, 'ENSG00000268592', 'RAET1E-AS1.2'), (13877, 'ENSG00000231963', 'AL662864.1'), (16117, 'ENSG00000225655', 'BX255923.1'), (16938, 'ENSG00000282955', 'RABL6.2'), (17241, 'ENSG00000265264', 'TIMM10B.2'), (18626, 'ENSG00000282682', 'C11orf71.2'), (18984, 'ENSG00000282883', 'AKR1C3.2'), (19226, 'ENSG00000150076', 'CCDC7.2'), (19346, 'ENSG00000264404', 'BX547991.1'), (21184, 'ENSG00000282031', 'TMBIM4.2'), (21230, 'ENSG00000257815', 'LINC01481.2'), (22033, 'ENSG00000228741', 'SPATA13.2'), (22037, 'ENSG00000281899', 'AL359736.3'), (22654, 'ENSG00000274827', 'LINC01297.2'), (23662, 'ENSG00000273259', 'AL049839.2'), (24019, 'ENSG00000211974', 'AC245369.1'), (26919, 'ENSG00000279257', 'C17orf100.2'), (26962, 'ENSG00000187838', 'PLSCR3'), (27137, 'ENSG00000255104', 'AC005324.4'), (27884, 'ENSG00000263715', 'LINC02210-CRHR1'), (28407, 'ENSG00000281844', 'FBF1.2'), (30440, 'ENSG00000283027', 'CAPS.2'), (32648, 'ENSG00000235271', 'LINC01422.2')]
+
+def read_10x_h5_file(input_h5, genome):
+	with tables.open_file(input_h5) as h5_in:
+		attrs = {}
+		for node in h5_in.walk_nodes("/" + genome, "Array"):
+			attrs[node.name] = node.read()
+
+	X = csr_matrix((attrs["data"], attrs["indices"], attrs["indptr"]), shape = (attrs["shape"][1], attrs["shape"][0]))
+	adata = anndata.AnnData(X = X, 
+							obs = {"obs_names" : attrs["barcodes"].decode()},
+                            var = {"var_names" : attrs["gene_names"].decode(),
+                             	   "gene_ids": attrs["genes"].decode()})
+
+	return adata
 
 def update_var_names(adata, genome):
 	if adata.var_names[0].startswith(genome + "_"):
@@ -32,20 +49,16 @@ def update_var_names(adata, genome):
 	
 	adata.var_names = pd.Index(gsyms)
 
-
-
 def cluster(input_name, output_name, **kwargs):
 	output_name = os.path.abspath(output_name)
 	output_dir = os.path.dirname(output_name)
 	output_base = '.' + os.path.basename(output_name)
 
-	sc.settings.verbosity = 3
-	sc.settings.writedir = sc.settings.figdir = output_dir + '/'
-
 	if not kwargs['input_h5ad']:
-		adata = sc.read_10x_h5(input_name + '_10x.h5', kwargs['genome'])
+		adata = read_10x_h5_file(input_name + '_10x.h5', kwargs['genome'])
 	else:
-		adata = sc.read(input_name + '.h5ad')
+		adata = anndata.read_h5ad(input_name + '.h5ad')
+	
 	update_var_names(adata, kwargs['genome'])
 
 	adata.obs['Channel'] = ['-'.join(x.split('-')[:-2]) for x in adata.obs_names]
@@ -55,11 +68,11 @@ def cluster(input_name, output_name, **kwargs):
 	mito_genes = [name for name in adata.var_names if name.startswith(kwargs['mito_prefix'])]
 	adata.obs['percent_mito'] = adata[:, mito_genes].X.sum(axis=1).A1 / adata.obs['n_counts'].values
 
-	if kwargs['diagnosis']:
-		sc.pl.violin(adata, ['n_genes', 'n_counts', 'percent_mito'], jitter=0.4, multi_panel=True, save=output_base)
-		sc.pl.scatter(adata, x='n_counts', y='percent_mito', save=output_base + ".1")
-		sc.pl.scatter(adata, x='n_counts', y='n_genes', save=output_base + ".2")
-		print("Plotted QC figures.")
+	# if kwargs['diagnosis']:
+	# 	sc.pl.violin(adata, ['n_genes', 'n_counts', 'percent_mito'], jitter=0.4, multi_panel=True, save=output_base)
+	# 	sc.pl.scatter(adata, x='n_counts', y='percent_mito', save=output_base + ".1")
+	# 	sc.pl.scatter(adata, x='n_counts', y='n_genes', save=output_base + ".2")
+	# 	print("Plotted QC figures.")
 
 	# Filter cells	
 	if kwargs['filtr_xlsx'] is not None:
@@ -123,9 +136,9 @@ def cluster(input_name, output_name, **kwargs):
 	# ~ 10^3, for 10^5, will include majority of expressed genes
 	# filter_result contains a boolean list (gene_subset) within robust genes
 	filter_result = batch_correction.filter_genes_dispersion(adata, kwargs['correct_batch'])
-	if kwargs['diagnosis']:
-		sc.pl.filter_genes_dispersion(filter_result, save=output_base)
-		print("Plotted variable gene plot.")
+	# if kwargs['diagnosis']:
+	# 	sc.pl.filter_genes_dispersion(filter_result, save=output_base)
+	# 	print("Plotted variable gene plot.")
 
 	adata_c = batch_correction.collect_variable_gene_matrix(adata, filter_result.gene_subset)
 
