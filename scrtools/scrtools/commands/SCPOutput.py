2b0257353c6187e32d0c1e8f57bb266ffadd2f11
Bo Li <libo@broadinstitute.org>
2018-07-18 15:07:16 -0400

    Updated SCP related codes

--- scrtools/scrtools/commands/SCPOutput.py
+++ scrtools/scrtools/commands/SCPOutput.py
@@ -6,7 +6,7 @@ class SCPOutput(Base):
 Generate outputs for single cell portal.
 
 Usage:
-  scrtools scp_output <input_h5ad_file> <output_name>
+  scrtools scp_output [--dense --round-to <ndigit>] <input_h5ad_file> <output_name>
   scrtools scp_output -h
 
 Arguments:
@@ -14,14 +14,16 @@ Arguments:
   output_name            Name prefix for all outputted files.
 
 Options:
+  --dense                Output dense expression matrix instead.
+  --round-to <ndigit>    Round expression to <ndigit> after the decimal point. [default: 2]
   -h, --help             Print out help information.
 
 Outputs:
-  output_name.scp.metadata.txt, output_name.scp.barcodes.tsv, output_name.scp.genes.tsv, output_name.scp.matrix.mtx, output_name.scp.*.coords.txt         Files that single cell portal needs.
+  output_name.scp.metadata.txt, output_name.scp.barcodes.tsv, output_name.scp.genes.tsv, output_name.scp.matrix.mtx, output_name.scp.*.coords.txt, output_name.scp.expr.txt         Files that single cell portal needs.
 
 Examples:
   scrtools scp_output manton_bm.h5ad manton_bm
     """
 
     def execute(self):
-        run_scp_output(self.args['<input_h5ad_file>'], self.args['<output_name>'])
+        run_scp_output(self.args['<input_h5ad_file>'], self.args['<output_name>'], not self.args['--dense'], int(self.args['--round-to']))
--- scrtools/scrtools/tools/scp_output.py
+++ scrtools/scrtools/tools/scp_output.py
@@ -1,5 +1,6 @@
+import numpy as np
 import pandas as pd
-import scipy.io as sio
+from scipy.sparse import issparse
 from .preprocessing import read_input
 
 def scp_write_coords(data, output_name):
@@ -42,22 +43,54 @@ def scp_write_metadata(data, output_name):
 	data.obs[meta].to_csv(metadata_file, sep = '\t', header = False, mode = 'a')
 	print("Metadata file {} is written.".format(metadata_file))
 
-def scp_write_expression(data, output_name):
-	barcode_file = "{}.scp.barcodes.tsv".format(output_name)
-	with open(barcode_file, 'w') as fout:
-		fout.write('\n'.join(data.obs_names) + '\n')
-	print("Barcode file {} is written.".format(barcode_file))
-	gene_file = "{}.scp.genes.tsv".format(output_name)
-	df = pd.DataFrame({'gene_names' : data.var_names, 'gene_ids' : data.var['gene_ids']})[['gene_ids', 'gene_names']]
-	with open(gene_file, 'w') as fout:
-		df.to_csv(fout, sep = ' ', header = False, index = False)
-	print("Gene file {} is written.".format(gene_file))
-	mtx_file = "{}.scp.matrix.mtx".format(output_name)
-	sio.mmwrite(mtx_file, data.X.transpose())
-	print("Matrix file {} is written.".format(mtx_file))
 
-def run_scp_output(input_h5ad_file, output_name):
+def write_market_matrix(mtx_file, X, round_to):
+	with open(mtx_file, 'w') as fout:
+		fmt_str = "{{}} {{}} {{:.{}f}}\n".format(round_to)
+		fout.write("%%MatrixMarket matrix coordinate real general\n%\n")
+		if issparse(X):
+			X = X.tocoo()
+			fout.write("{} {} {}\n".format(X.shape[0], X.shape[1], (X.data != 0).sum()))
+			for x, y, value in zip(X.row, X.col, X.data):
+				if value != 0:
+					fout.write(fmt_str.format(x + 1, y + 1, value))
+		else:
+			fout.write("{} {} {}\n".format(X.shape[0], X.shape[1], np.count_nonzero(X)))
+			for x, y in zip(*X.nonzero()):
+				fout.write(fmt_str.format(x + 1, y + 1, X[x, y]))
+
+def write_dense_matrix(expr_file, data, round_to):
+	fmt_str = "{{:.{}f}}".format(round_to)
+	exprmat = data.X.transpose()
+	if issparse(exprmat):
+		exprmat = exprmat.toarray()
+	with open(expr_file, 'w') as fout:
+		fout.write("GENE\t" + "\t".join(data.obs_names) + "\n")
+		for i in range(exprmat.shape[0]):
+			fout.write(data.var_names[i] + "\t" + "\t".join([fmt_str.format(x) if x > 0.0 else "0" for x in exprmat[i,:]]) + "\n")
+	print("Expression matrix {} is written.".format(expr_file))
+
+
+def scp_write_expression(data, output_name, is_sparse = True, round_to = 2):
+	if is_sparse:
+		barcode_file = "{}.scp.barcodes.tsv".format(output_name)
+		with open(barcode_file, 'w') as fout:
+			fout.write('\n'.join(data.obs_names) + '\n')
+		print("Barcode file {} is written.".format(barcode_file))
+		gene_file = "{}.scp.genes.tsv".format(output_name)
+		df = pd.DataFrame({'gene_names' : data.var_names, 'gene_ids' : data.var['gene_ids']})[['gene_ids', 'gene_names']]
+		with open(gene_file, 'w') as fout:
+			df.to_csv(fout, sep = ' ', header = False, index = False)
+		print("Gene file {} is written.".format(gene_file))
+		mtx_file = "{}.scp.matrix.mtx".format(output_name)
+		write_market_matrix(mtx_file, data.X.transpose(), round_to)
+		print("Matrix file {} is written.".format(mtx_file))
+	else:
+		expr_file = "{}.scp.expr.txt".format(output_name)
+		write_dense_matrix(expr_file, data, round_to)
+
+def run_scp_output(input_h5ad_file, output_name, is_sparse, round_to):
 	adata = read_input(input_h5ad_file, is_raw = False, load_all = True)
 	scp_write_coords(adata, output_name)
 	scp_write_metadata(adata, output_name)
-	scp_write_expression(adata, output_name)
+	scp_write_expression(adata, output_name, is_sparse, round_to)
