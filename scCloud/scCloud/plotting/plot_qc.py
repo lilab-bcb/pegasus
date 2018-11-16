90e667ca45c8a79330a077603c095834e421c167
Bo Li <libo@broadinstitute.org>
2018-11-16 13:20:14 -0500

    Updated

--- scCloud/scCloud/plotting/plot_qc.py
+++ scCloud/scCloud/plotting/plot_qc.py
@@ -7,13 +7,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # plot_type: gene, count, mito
-def plot_qc_violin(data, plot_type, out_file, xattr = 'Channel', hue = None, inner = None, dpi = 500, figsize = None, xlabel = None, xtick_font = None, xtick_rotation = False, no_legend = False):
+def plot_qc_violin(data, plot_type, out_file, xattr = 'Channel', hue = None, inner = None, dpi = 500, figsize = None, xlabel = None, xtick_font = None, xtick_rotation = False, no_legend = False, split = False):
 	pt2attr = {'gene' : 'n_genes', 'count' : 'n_counts', 'mito' : 'percent_mito'}
 	pt2ylab = {'gene' : 'Number of expressed genes', 'count' : 'Number of UMIs', 'mito' : 'Percentage of mitochondrial genes'}
 
 	yattr = pt2attr[plot_type]
 
-	split = True if hue is not None else False
 	df = data.obs[[xattr, yattr]] if hue is None else data.obs[[xattr, yattr, hue]]
 
 	sns.violinplot(x = xattr, y = yattr, hue = hue, data = df, inner = inner, split = split)
