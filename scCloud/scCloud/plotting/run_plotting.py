09ba92868827baef732f91e1f5707c766911977a
Bo Li <libo@broadinstitute.org>
2019-04-21 16:57:51 -0400

    Fixed a bug'

--- scCloud/scCloud/plotting/run_plotting.py
+++ scCloud/scCloud/plotting/run_plotting.py
@@ -9,13 +9,15 @@ from . import plot_library, iplot_library
 
 
 pop_list = {
-	'composition' : {'basis', 'attrs', 'apply_to_all', 'group', 'genes', 'gene', 'nrows', 'ncols', 'alpha', 'legend_fontsize', 'use_raw', 'showzscore', 'title', 'showall', 'show_background'},
-	'scatter' : {'cluster', 'attr', 'group', 'genes', 'gene', 'style', 'stacked', 'logy', 'use_raw', 'showzscore', 'title', 'showall'},
-	'scatter_groups' : {'attr', 'attrs', 'apply_to_all', 'genes', 'gene', 'style', 'stacked', 'logy', 'use_raw', 'showzscore', 'title', 'show_background'},
-	'scatter_genes' : {'cluster', 'attr', 'attrs', 'restrictions', 'apply_to_all', 'group', 'gene', 'style', 'stacked', 'logy', 'legend_fontsize', 'showzscore', 'title', 'showall', 'show_background'},
-	'scatter_gene_groups' : {'cluster', 'attr', 'attrs', 'restrictions', 'apply_to_all', 'genes', 'style', 'stacked', 'logy', 'legend_fontsize', 'showzscore', 'title', 'showall', 'show_background'},
-	'heatmap' : {'attr', 'basis', 'attrs', 'restrictions', 'apply_to_all', 'group', 'gene', 'style', 'stacked', 'logy', 'nrows', 'ncols', 'subplot_size', 'left', 'bottom', 'wspace', 'hspace', 'alpha', 'legend_fontsize', 'showall', 'show_background'}
+	'composition' : {'basis', 'attrs', 'apply_to_all', 'group', 'genes', 'gene', 'nrows', 'ncols', 'alpha', 'legend_fontsize', 'use_raw', 'showzscore', 'title', 'showall', 'show_background', 'qc_type', 'qc_xtick_font', 'qc_xtick_rotation', 'qc_line_width'},
+	'scatter' : {'cluster', 'attr', 'group', 'genes', 'gene', 'style', 'stacked', 'logy', 'use_raw', 'showzscore', 'title', 'showall', 'qc_type', 'qc_xtick_font', 'qc_xtick_rotation', 'qc_line_width'},
+	'scatter_groups' : {'attr', 'attrs', 'apply_to_all', 'genes', 'gene', 'style', 'stacked', 'logy', 'use_raw', 'showzscore', 'title', 'show_background', 'qc_type', 'qc_xtick_font', 'qc_xtick_rotation', 'qc_line_width'},
+	'scatter_genes' : {'cluster', 'attr', 'attrs', 'restrictions', 'apply_to_all', 'group', 'gene', 'style', 'stacked', 'logy', 'legend_fontsize', 'showzscore', 'title', 'showall', 'show_background', 'qc_type', 'qc_xtick_font', 'qc_xtick_rotation', 'qc_line_width'},
+	'scatter_gene_groups' : {'cluster', 'attr', 'attrs', 'restrictions', 'apply_to_all', 'genes', 'style', 'stacked', 'logy', 'legend_fontsize', 'showzscore', 'title', 'showall', 'show_background', 'qc_type', 'qc_xtick_font', 'qc_xtick_rotation', 'qc_line_width'},
+	'heatmap' : {'attr', 'basis', 'attrs', 'restrictions', 'apply_to_all', 'group', 'gene', 'style', 'stacked', 'logy', 'nrows', 'ncols', 'subplot_size', 'left', 'bottom', 'wspace', 'hspace', 'alpha', 'legend_fontsize', 'showall', 'show_background', 'qc_type', 'qc_xtick_font', 'qc_xtick_rotation', 'qc_line_width'}
 }
+
+
 
 def make_static_plots(input_file, plot_type, output_file, dpi = 500, **kwargs):
 	adata = read_input(input_file, mode = 'r')
