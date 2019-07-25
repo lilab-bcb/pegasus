1aad63261dd0480b3ef3e751eb070eb2ab2c0e6e
Yiming Yang <yang.yihming@gmail.com>
2019-07-25 16:36:42 -0400

    restore defaults of run_net_fle()

--- scCloud/tools/visualization.py
+++ scCloud/tools/visualization.py
@@ -179,7 +179,7 @@ def run_net_umap(data, rep_key, selected, n_jobs, n_components = 2, n_neighbors 
 	end = time.time()
 	print("Net UMAP is calculated. Time spent = {:.2f}s.".format(end - start))
 
-def run_net_fle(data, selected, file_name, n_jobs, K = 50, target_change_per_node = 2.0, target_steps = 5000, is3d = False, memory = 8, random_state = 0, \
+def run_net_fle(data, selected, file_name, n_jobs, K = 50, target_change_per_node = 2.0, target_steps = 10000, is3d = False, memory = 8, random_state = 0, \
 	ds_full_speed = False, net_alpha = 0.1, polish_target_steps = 1500, out_basis = 'net_fle'):
 	start = time.time()
 	X = data.obsm['X_diffmap'][selected,:]
