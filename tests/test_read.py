a20c6ab8ddcd9ef149cae4682c21c4edce3151fd
jgould <jgould@broadinstitute.org>
2019-08-06 14:21:36 -0400

    updated paths

--- tests/test_read.py
+++ tests/test_read.py
@@ -7,25 +7,25 @@ from .test_util import assert_adata_equal
 class TestRead(unittest.TestCase):
 
     def test_mtx_v2(self):
-        adata = sc.tools.read_input('scCloud-test-data/tests/input/hgmm_1k_filtered_gene_bc_matrices/hg19/matrix.mtx')
+        adata = sc.io.read_input('tests/scCloud-test-data/input/hgmm_1k_filtered_gene_bc_matrices/hg19/matrix.mtx')
         self.assertEqual(adata.shape[0], 504)
 
     def test_mtx_v3(self):
-        adata = sc.tools.read_input('scCloud-test-data/tests/input/hgmm_1k_v3_filtered_feature_bc_matrix/matrix.mtx.gz')
+        adata = sc.io.read_input('tests/scCloud-test-data/input/hgmm_1k_v3_filtered_feature_bc_matrix/matrix.mtx.gz')
         self.assertEqual(adata.shape[0], 1046)
 
     def test_mtx_v2_dir(self):
-        adata = sc.tools.read_input('scCloud-test-data/tests/input/hgmm_1k_filtered_gene_bc_matrices/hg19/')
+        adata = sc.io.read_input('tests/scCloud-test-data/input/hgmm_1k_filtered_gene_bc_matrices/hg19/')
         self.assertEqual(adata.shape[0], 504)
 
     def test_mtx_v3_dir(self):
-        adata = sc.tools.read_input('scCloud-test-data/tests/input/hgmm_1k_v3_filtered_feature_bc_matrix/')
+        adata = sc.io.read_input('tests/scCloud-test-data/input/hgmm_1k_v3_filtered_feature_bc_matrix/')
         self.assertEqual(adata.shape[0], 1046)
 
     def test_read_write_h5ad(self):
-        adata = sc.tools.read_input('scCloud-test-data/tests/input/hgmm_1k_v3_filtered_feature_bc_matrix/')
-        sc.tools.write_output(adata, 'test.h5ad')
-        adata2 = sc.tools.read_input('test.h5ad', mode='a')
+        adata = sc.io.read_input('tests/scCloud-test-data/input/hgmm_1k_v3_filtered_feature_bc_matrix/')
+        sc.io.write_output(adata, 'test.h5ad')
+        adata2 = sc.io.read_input('test.h5ad', h5ad_mode='a')
         assert_adata_equal(self, adata, adata2)
 
 
