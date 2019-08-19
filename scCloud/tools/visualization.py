8ce4c4a7ebee25f303b69c8e889a9eb532b72c97
Yiming Yang <yang.yihming@gmail.com>
2019-08-19 14:43:16 -0400

    move force_directed_layout code to forceatlas-python

--- scCloud/tools/visualization.py
+++ scCloud/tools/visualization.py
@@ -10,6 +10,7 @@ import pkg_resources
 
 from MulticoreTSNE import MulticoreTSNE as TSNE
 import umap as umap_module
+import forceatlas2 as fa2
 
 from typing import List
 
@@ -223,68 +224,9 @@ def calc_force_directed_layout(
     """
     TODO: Typing
     """
-    input_graph_file = "{file_name}.net".format(file_name=file_name)
     G = construct_graph(W)
-    G.write(input_graph_file)
-    print(input_graph_file + " is written.")
-
-    output_coord_file = "{file_name}.coords.txt".format(file_name=file_name)
-
-    classpath = (
-        pkg_resources.resource_filename("scCloud", "ext/forceatlas2.jar")
-        + ":"
-        + pkg_resources.resource_filename("scCloud", "ext/gephi-toolkit-0.9.2-all.jar")
-    )
-    command = [
-        "java",
-        "-Djava.awt.headless=true",
-        "-Xmx{memory}g".format(memory=memory),
-        "-cp",
-        classpath,
-        "kco.forceatlas2.Main",
-        "--input",
-        input_graph_file,
-        "--output",
-        file_name + ".coords",
-        "--nthreads",
-        str(n_jobs),
-        "--seed",
-        str(random_state),
-        "--targetChangePerNode",
-        str(target_change_per_node),
-        "--targetSteps",
-        str(target_steps),
-    ]
-    if not is3d:
-        command.append("--2d")
-
-    if init is not None:
-        if not is3d:
-            df = pd.DataFrame(
-                data=init, columns=["x", "y"], index=range(1, init.shape[0] + 1)
-            )
-        else:
-            df = pd.DataFrame(
-                data=init, columns=["x", "y", "z"], index=range(1, init.shape[0] + 1)
-            )
-        df.index.name = "id"
-        init_coord_file = "{file_name}.init.coords.txt".format(file_name=file_name)
-        df.to_csv(init_coord_file, sep="\t", float_format="%.2f")
-        command.extend(["--coords", init_coord_file])
-
-    print(command)
-    check_call(command)
-
-    fle_coords = pd.read_csv(output_coord_file, header=0, index_col=0, sep="\t").values
-
-    check_call(["rm", "-f", input_graph_file])
-    check_call(["rm", "-f", output_coord_file])
-    if init is not None:
-        os.remove("{file_name}.init.coords.txt".format(file_name=file_name))
-
-    print("Force-directed layout is generated.")
-
-    return fle_coords
+    return fa2.forceatlas2(file_name, graph = G, n_jobs = n_jobs, target_change_per_node = target_change_per_node, \
+        target_steps = target_steps, is3d = is3d, memory = memory, random_state = random_state, init = init)
 
 
 def tsne(
