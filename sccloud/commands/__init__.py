fa31b31b4e563071e0453db376e645f28221708b
Yiming Yang <yang.yihming@gmail.com>
2019-08-24 15:33:47 -0400

    Add parquet and scp_output commands back

--- sccloud/commands/__init__.py
+++ sccloud/commands/__init__.py
@@ -7,6 +7,8 @@ from .Plotting import Plotting as plot
 from .iPlotting import iPlotting as iplot
 from .SubClustering import SubClustering as subcluster
 from .View import View as view
+from .PARQUET import PARQUET as parquet
+from .SCPOutput import SCPOutput as scp_output
 from .CITESeq import CITESeq as merge_rna_adt
 from .CheckSampleIndexes import CheckSampleIndexes as check_indexes
 from .FindMarkers import FindMarkers as find_markers
