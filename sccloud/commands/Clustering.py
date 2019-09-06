7a5589d77410744b9206bb9d630c146045a6f47e
Yiming Yang <yang.yihming@gmail.com>
2019-09-06 16:15:34 -0400

    Modify documentation slightly to clarify possible confusions

--- sccloud/commands/Clustering.py
+++ sccloud/commands/Clustering.py
@@ -37,8 +37,8 @@ Options:
   --min-umis <number>                              Only keep cells with at least <number> of UMIs. [default: 100]
   --max-umis <number>                              Only keep cells with less than <number> of UMIs. [default: 600000]
   --mito-prefix <prefix>                           Prefix for mitochondrial genes. If multiple prefixes are provided, separate them by comma (e.g. "MT-,mt-"). [default: MT-]
-  --percent-mito <percent>                         Only keep cells with mitochondrial percent less than <percent>. [default: 10.0]
-  --gene-percent-cells <percent>                   Only use genes that are expressed in at least <percent> percent of cells to select variable genes. [default: 0.05]
+  --percent-mito <percent>                         Only keep cells with mitochondrial percent less than <percent>%. [default: 10.0]
+  --gene-percent-cells <percent>                   Only use genes that are expressed in at least <percent>% of cells to select variable genes. [default: 0.05]
 
   --counts-per-cell-after <number>                 Total counts per cell after normalization. [default: 1e5]
 
