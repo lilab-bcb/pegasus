6aa116b24ad3176340eceacd11a74312e48322e3
jgould <jgould@broadinstitute.org>
2019-09-11 11:08:16 -0400

    update bokeh for scplot

--- scripts_install_sccloudpy.sh
+++ scripts_install_sccloudpy.sh
@@ -5,8 +5,11 @@ set -e
 apt-get update
 apt-get install --no-install-recommends -y libfftw3-dev
 pip3 install --upgrade pip
+pip3 install --upgrade bokeh
 pip3 install cmake
 pip3 install Cython
+pip3 install --upgrade numpy
 pip3 install MulticoreTSNE-modified==0.1.post2
 pip3 install sccloud[fitsne]==0.14.0rc2
+
 echo "sccloud done"
