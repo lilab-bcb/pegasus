ab1ca18f0db36325aad202dfa46cf04eeb567b71
Tariq Daouda <tariq.daouda@umontreal.ca>
2019-10-28 13:56:41 -0400

    optimized get_neighbors memory usage

--- pegasus/tools/nearest_neighbors.py
+++ pegasus/tools/nearest_neighbors.py
@@ -146,8 +146,10 @@ def get_symmetric_matrix(csr_mat: "csr_matrix") -> "csr_matrix":
     sym_mat = csr_mat + tp_mat
     sym_mat.sort_indices()
 
-    idx_mat = (csr_mat != 0).astype(int) + (tp_mat != 0).astype(int)
-    idx_mat.sort_indices()
+    # idx_mat = (csr_mat != 0).astype(int) + (tp_mat != 0).astype(int)
+    idx_mat = csr_mat.indptr & tp_mat.indptr
+    # idx_mat.sort_indices()
+    idx_mat.sort()
     idx = idx_mat.data == 2
 
     sym_mat.data[idx] /= 2.0
