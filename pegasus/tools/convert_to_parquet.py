3712ffd59cb3c629b0613ec11b92d9fec6ae28ea
jgould <jgould@broadinstitute.org>
2019-11-13 09:58:02 -0500

    Updated slicing backed anndata object to avoid memory issues

--- pegasus/tools/convert_to_parquet.py
+++ pegasus/tools/convert_to_parquet.py
@@ -8,7 +8,6 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import scipy.sparse
 from pegasus.io import read_input
-
 from pegasus.utils import decorators as pg_deco
 
 obsm_whitelist = ['X_pca', 'X_rpca', 'X_tsne', 'X_fitsne', 'X_umap', 'X_fle', 'X_net_tsne', 'X_net_umap', 'X_net_fle']
@@ -46,25 +45,35 @@ def create_schema(data):
     return schema
 
 
-def to_df(data):
-    X = data.X
+def to_df(data, obs_subset=None):
+    X = data.X if obs_subset is None else data.X[obs_subset]
     if scipy.sparse.issparse(X):
         X = X.toarray()
 
     if str(X.dtype) != 'float32':
         X = X.astype('float32')
 
-    df = pd.DataFrame(index=data.obs.index, data=X, columns=data.var_names)
+    obs = data.obs if obs_subset is None else data.obs.iloc[obs_subset]
+    df = pd.DataFrame(index=obs.index, data=X, columns=data.var_names)
     for key in data.obsm.keys():
+        m = data.obsm[key] if obs_subset is None else data.obsm[key][obs_subset]
+
         if key in obsm_whitelist:
-            df["{}_1".format(key)] = data.obsm[key][:, 0].astype('float32')
-            df["{}_2".format(key)] = data.obsm[key][:, 1].astype('float32')
+            df["{}_1".format(key)] = m[:, 0].astype('float32')
+            df["{}_2".format(key)] = m[:, 1].astype('float32')
         elif key in obsm_whitelist_3d:
-            df["{}_1".format(key)] = data.obsm[key][:, 0].astype('float32')
-            df["{}_2".format(key)] = data.obsm[key][:, 1].astype('float32')
-            df["{}_3".format(key)] = data.obsm[key][:, 2].astype('float32')
+            df["{}_1".format(key)] = m[:, 0].astype('float32')
+            df["{}_2".format(key)] = m[:, 1].astype('float32')
+            df["{}_3".format(key)] = m[:, 2].astype('float32')
     df = df.join(data.obs)
     return df
+
+
+def fix_categorical_dtypes(df):
+    for column in df:
+        if pd.api.types.is_categorical_dtype(df[column]) and len(df[column].dtype.categories) >= 10000:
+            print('Converted {} to string'.format(column))
+            df[column] = df[column].astype(str)
 
 
 @pg_deco.TimeLogger()
@@ -76,17 +85,18 @@ def convert_to_parquet(data, output_name, nthreads, row_group_size):
         for i in range(0, data.shape[0], row_group_size):
             end = i + row_group_size
             end = min(end, data.shape[0])
-            df = to_df(data[i:end])
-            print('{}-{}'.format(i, end))
+            df = to_df(data, slice(i, end))
+            print('Converting {}-{}'.format(i, end))
             table = pa.Table.from_pandas(df, schema=schema, nthreads=nthreads)
             writer.write_table(table)
             gc.collect()
     print(output_name + " is written!")
 
 
-def run_conversion(input_h5ad_file, output_name, nthreads, n_row_groups=None):
-    data = read_input(input_h5ad_file)
+def run_conversion(input_h5ad_file, output_name, nthreads, backed: bool = False, n_row_groups=None):
+    data = read_input(input_h5ad_file, h5ad_mode='r' if backed else 'a')
+    fix_categorical_dtypes(data.obs)
     if n_row_groups is None:
-        n_row_groups = min(1, math.ceil(data.shape[0] / 1000000))
+        n_row_groups = min(1, math.ceil(data.shape[0] / 100000))
     row_group_size = math.ceil(data.shape[0] / n_row_groups)
     convert_to_parquet(data, output_name, nthreads, row_group_size)
