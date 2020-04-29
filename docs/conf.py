88dfcd4dba2b2086abc27f8bd2366c40bf7d3fc6
Yiming Yang <yang.yihming@gmail.com>
2020-04-29 14:45:13 -0400

    clean up

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ author = (
 )
 
 # The short X.Y version
-version = "0.15.0"
+version = "0.17"
 # The full version, including alpha/beta/rc tags
-release = "0.15.0"
+release = "0.17.1"
 
 
 # -- General configuration ---------------------------------------------------
