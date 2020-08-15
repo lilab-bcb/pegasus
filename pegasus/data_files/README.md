d1e97b295872c842edf0adb622a190c260769721
Bo Li <bli28@mgh.harvard.edu>
2020-08-15 15:06:13 -0400

    Tested the signature score function

--- pegasus/data_files/README.md
+++ pegasus/data_files/README.md
@@ -4,5 +4,5 @@
 4) Mitocondrial genes mouse are collected from GENCODE gencode.vM25.primary_assembly.annotation.gtf / "chrM"/"gene"/"protein_coding"; also find mitocondrial ribosomal genes by Mrpl and Mrps
 5) Ribosomal genes human are collected from GENCODE gencode.v34.primary_assembly.annotation.gtf / "gene"/"protein_coding"/"\"RPL"/"\"RPS"; also refer to Kenmochi et al. Genome Res 1998 & Yoshihama et al. Genome Res 2002. 78 in total.
 6) Ribosomal genes mouse are collected from GENCODE gencode.vM25.primary_assembly.annotation.gtf / "gene"/"protein_coding"/"\Rpl"/"\"Rps". 78 in total.
-7) Gender-specific genes for human: female XIST + other genes from https://bmcbiol.biomedcentral.com/articles/10.1186/s12915-017-0352-z/figures/3. Male: all protein coding / lncRNA genes without PAR tag from GENCODE gencode.v34.primary_assembly.annotation.gtf.
-8) Gender-specific genes for mouse: female Xist + other genes mapped from human genes. Male: all protein coding / lincRNA genes without PAR tag from GENCODE gencode.vM25.primary_assembly.annotation.gtf.
+7) Gender-specific genes for human: female XIST, DONSON, RPS4X. Male: selected genes from chrY from GENCODE gencode.v34.primary_assembly.annotation.gtf.
+8) Gender-specific genes for mouse: Mapped from human symbols.
--- pegasus/tools/signature_score.py
+++ pegasus/tools/signature_score.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
 from sklearn.cluster import KMeans
 
+import anndata
 from pegasusio import UnimodalData, MultimodalData
 
 import logging
@@ -66,7 +67,7 @@ def _load_signatures_from_file(input_file: str) -> Dict[str, List[str]]:
     return signatures
 
 
-def _calc_sig_scores(data: UnimodalData, signatures: Dict[str, List[str]], show_omitted_genes: bool = False, skip_threshold: int = 2) -> None:
+def _calc_sig_scores(data: UnimodalData, signatures: Dict[str, List[str]], show_omitted_genes: bool = False, skip_threshold: int = 1) -> None:
     for key, gene_list in signatures.items():
         genes = pd.Index(gene_list)
         idx = data.var_names.isin(genes)
@@ -86,22 +87,78 @@ def _calc_sig_scores(data: UnimodalData, signatures: Dict[str, List[str]], show_
             data.obs[key] = (data.X[:, idx].mean(axis = 1).A1 - data.var.loc[idx, "mean"].mean()) - data.obsm["sig_background"][:, data.var["bins"].cat.codes[idx]].mean(axis = 1)
 
 
+def _estimate_null(maxvalues: List[float], right_cutoff: int = 0.2) -> List[bool]:
+    from scipy.stats import gaussian_kde
+    kde_f = gaussian_kde(maxvalues)
+    x = np.linspace(maxvalues.min(), min(maxvalues.max(), right_cutoff))
+    y = kde_f(x)
+    # detect peaks
+    peak_pos = []
+    for i in range(1, x.size - 1):
+        if y[i] > y[i - 1] and y[i] > y[i + 1]:
+            peak_pos.append(i)
+    #
+    assert len(peak_pos) > 0
+    if len(peak_pos) > 2:
+        logger.warning("Interesting! We detected more than 2 peaks. If you have time, please contact Cumulus/Pegasus team to investigate this case.")
+    #
+    if len(peak_pos) == 1:
+        mu = x[peak_pos[0]]
+    else:
+        ords = np.argsort(y[peak_pos])[::-1]
+        mu = (x[peak_pos[ords[0]]] + x[peak_pos[ords[1]]]) / 2.0
+    #
+    idx = maxvalues < mu
+    s = idx.sum()
+    if s > 0:
+        std = np.sqrt(((maxvalues[idx] - mu) ** 2).sum() / s)
+        return maxvalues <= mu + std * 3.0 # 3 sigma rule
+    else:
+        return np.zeros(maxvalues.size, dtype = bool)
+
+
+
+
 
 @timer(logger=logger)
-def calc_signature_score(data: MultimodalData, signatures: Union[Dict[str, List[str]], str], n_bins: int = 50, show_omitted_genes: bool = False, random_state: int = 0) -> None:
+def calc_signature_score(
+    data: Union[MultimodalData, UnimodalData, anndata.AnnData], 
+    signatures: Union[Dict[str, List[str]], str], 
+    n_bins: Optional[int] = 50, 
+    show_omitted_genes: Optional[bool] = False, 
+    random_state: Optional[int] = 0
+) -> None:
     """Calculate signature / gene module score.
 
     This is an improved version of Livnat et al. 2018 Cell implementation.
 
     Parameters
     ----------
-    data: ``anndata.AnnData``
-        MultimodalData with the current selected UnimodalData used.
+    data: ``MultimodalData``, ``UnimodalData``, or ``anndata.AnnData`` object.
+        Single cell expression data.
+
 
     signatures: ``Dict[str, List[str]]`` or ``str``
-        A dictionary containing multiple signature score calculation requests. Each score will be stored in data.obs field with key as the keyword. The value of the dict is a list of gene symbols. If <signatures> is a string, load signatures from the corresponding Gene Matrix Transposed (GMT)-formatted file. 
+        This argument accepts either a dictionary or a string.
+        If ``signatures`` is a dictionary, it can contain multiple signature score calculation requests. Each key in the dictionary represents a separate signature score calculation and its corresponding value contains a list of gene symbols. Each score will be stored in data.obs field with key as the keyword.
+        If ``signatures`` is a string, it should refer to a Gene Matrix Transposed (GMT)-formatted file. Pegasus will load signatures from the GMT file.
+        Pegasus also provide 4 default signature panels for each of human and mouse. They are ``cell_cycle_human``, ``gender_human``, ``mitochondrial_genes_human`` and ``ribosomal_genes_human`` for human; ``cell_cycle_mouse``, ``gender_mouse``, ``mitochondrial_genes_mouse`` and ``ribosomal_genes_mouse`` for mouse.
+        
+            * ``cell_cycle_human`` contains two cell-cycle signatures, ``G1/S`` and ``G2/M``, obtained from Tirosh et al. 2016. We also updated gene symbols according to Seurat's ``cc.genes.updated.2019`` vector. We additionally calculated signature score ``cycle_diff``, which is ``G2/M`` - ``G1/S``. We provide predicted cell cycle phases in ``data.obs['predicted_phase']`` in case it is useful. ``predicted_phase`` is predicted based on ``G1/S`` and ``G2/M`` scores. First, we identify ``G0`` cells. We calculate vector ``maxvalues`` as the maximum score of ``G1/S`` and ``G2/M`` for each cell and then estimate the distribution underneath ``maxvalues`` using scipy.stats.gaussian_kde. Based on the estimated density function we can identify the peak(s) (i.e. mu) and estimate standard deviation (i.e. sigma) of the null (``G0``) distribution. We set any cell with maxvalues <= mu + 3 * sigma as ``G0`` cells. For each other cell, if ``G1/S`` > ``G2/M``, it is a ``G1/S`` cell, otherwise it is a ``G2/M`` cell.
+            * ``gender_human`` contains two gender-specific signatures, ``female_score`` and ``male_score``. Genes were selected based on DE analysis between genders based on 8 channels of bone marrow data from HCA Census of Immune Cells and the brain nuclei data from Gaublomme and Li et al, 2019, Nature Communications. After calculation, three signature scores will be calculated: ``female_score``, ``male_score`` and ``gender_score``. ``female_score`` and ``male_score`` are calculated based on female and male signatures respectively and a larger score represent a higher likelihood of that gender. ``gender_score`` is calculated as ``male_score`` - ``female_score``. A large positive score likely represents male and a large negative score likely represents female. Pegasus also provides predicted gender for each cell based on ``gender_score``, which is stored in ``data.obs['predicted_gender']``. To predict genders, we apply the KMeans algorithm to the ``gender_score`` and ask for 3 clusters. The clusters with a minimum and maximum clauster centers are predicted as ``female`` and ``male`` respectively and the cluster in the middle is predicted as ``uncertain``. Note that this approach is conservative and it is likely that users can predict genders based on ``gender_score`` for cells in the ``uncertain`` cluster with a reasonable accuracy.
+            * ``mitochondrial_genes_human`` contains two signatures, ``mito_genes`` and ``mito_ribo``. ``mito_genes`` contains 13 mitocondrial genes from chrM and ``mito_ribo`` contains mitocondrial ribosomal genes that are not from chrM. Note that ``mito_genes`` correlates well with percent of mitocondrial UMIs and ``mito_ribo`` does not.
+            * ``ribosomal_genes_human`` contains one signature, ``ribo_genes``, which are ribosomal genes from both large and small units.
+
+            * ``cell_cycle_mouse``, ``gender_mouse``, ``mitochondrial_genes_mouse`` and ``ribosomal_genes_mouse`` are the corresponding signatures for mouse. Gene symbols are directly translated from human genes.
 
     n_bins: ``int``, optional, default: 50
+        Number of bins on expression levels for grouping genes
+
+    show_omitted_genes: ``bool``, optional, default False
+        Signature genes that are not expressed in the data will be omitted. By default, pegasus does not report which genes are omitted. If this option is turned on, report omitted genes.
+    
+    random_state: ``int``, optional, default: 0
+        Random state used by KMeans if signature == ``gender_human`` or ``gender_mouse``.
 
     Returns
     -------
@@ -128,6 +185,7 @@ def calc_signature_score(data: MultimodalData, signatures: Union[Dict[str, List[
     Examples
     --------
     >>> pg.calc_signature_score(data, {"T_cell_sig": ["CD3D", "CD3E", "CD3G", "TRAC"]})
+    >>> pg.calc_signature_score(data, "cell_cycle_human")
     """
     if isinstance(data, MultimodalData):
         data = data._unidata
@@ -141,23 +199,29 @@ def calc_signature_score(data: MultimodalData, signatures: Union[Dict[str, List[
             signatures = _load_signatures_from_file(predefined_signatures[sig_string])
             if sig_string.startswith("cell_cycle"):
                 _calc_sig_scores(data, signatures, show_omitted_genes = show_omitted_genes)
-                kmeans = KMeans(n_clusters=3, random_state=random_state)
-                kmeans.fit(data.obs[["G1/S", "G2/M"]].values)
-                remap_code = np.zeros(3, dtype = np.int32)
-                remap_code[1:] = np.argmax(kmeans.cluster_centers_, axis = 0)
-                assert remap_code[1] != remap_code[2]
-                remap_code[0] = 3 - remap_code[1:].sum()
-                codes = remap_code[kmeans.labels_]
-                data.obs["phase"] = pd.Categorical.from_codes(codes, categories = ["G0", "G1/S", "G2/M"])
+                data.obs["cycle_diff"] = data.obs["G2/M"] - data.obs["G1/S"]
+                
+                values = data.obs[["G1/S", "G2/M"]].values
+                maxvalues = values.max(axis = 1)
+
+                null_idx = _estimate_null(maxvalues)
+                cycle_idx = ~null_idx
+
+                codes = np.zeros(data.shape[0], dtype = np.int32)
+                codes[cycle_idx & (values[:, 0] == maxvalues)] = 1
+                codes[cycle_idx & (values[:, 1] == maxvalues)] = 2
+
+                data.obs["predicted_phase"] = pd.Categorical.from_codes(codes, categories = ["G0", "G1/S", "G2/M"])
             elif sig_string.startswith("gender"):
                 _calc_sig_scores(data, signatures, show_omitted_genes = show_omitted_genes, skip_threshold = 1)
                 data.obs["gender_score"] = data.obs["male_score"] - data.obs["female_score"]
-                kmeans = KMeans(n_clusters=2, random_state=random_state)
+
+                kmeans = KMeans(n_clusters=3, random_state=random_state)
                 kmeans.fit(data.obs["gender_score"].values.reshape(-1, 1))
-                codes = kmeans.labels_
-                if kmeans.cluster_centers_[0, 0] > kmeans.cluster_centers_[1, 0]:
-                    codes = 1 - codes # 0: female; 1: male
-                data.obs["gender"] = pd.Categorical.from_codes(codes, categories = ["female", "male"])
+                reorg_dict = {y:x for x, y in enumerate(np.argsort(kmeans.cluster_centers_[:,0]))}
+                codes = list(map(lambda x: reorg_dict[x], kmeans.labels_))
+                
+                data.obs["predicted_gender"] = pd.Categorical.from_codes(codes, categories = ["female", "uncertain", "male"])
             elif sig_string.startswith("mitochondrial_genes"):
                 del signatures["mito_noncoding"]
                 _calc_sig_scores(data, signatures, show_omitted_genes = show_omitted_genes, skip_threshold = 1)
