3e505ef9a8eb48db77e7bf8e70be5196c4328adc
Yiming Yang <yang.yihming@gmail.com>
2020-09-03 21:14:11 -0400

    fix bug

--- pegasus/annotate_cluster/annotate_cluster.py
+++ pegasus/annotate_cluster/annotate_cluster.py
@@ -152,14 +152,14 @@ class Annotator:
         results = []
         for celltype in obj["cell_types"]:
             ct = CellType(celltype["name"], ignore_nonde=ignore_nonde)
-            ct.evaluate(celltype, de_up, de_down, threshold)
+            ct.evaluate(celltype, de_up, de_down, fc_thre)
             if ct.score >= threshold:
                 sub_obj = celltype.get("subtypes", None)
                 if sub_obj is not None:
                     ct.subtypes = self.evaluate(
                         de_up,
                         de_down,
-                        fc_thre=threshold,
+                        fc_thre=fc_thre,
                         ignore_nonde=ignore_nonde,
                         obj=sub_obj,
                     )
