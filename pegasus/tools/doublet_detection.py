46aeccc3846ecd63076d82b5102f97851ebf4729
Bo Li <bli28@mgh.harvard.edu>
2020-09-11 00:48:43 -0400

    Added min_dbl_rate for doublet detection

--- pegasus/tools/doublet_detection.py
+++ pegasus/tools/doublet_detection.py
@@ -23,12 +23,14 @@ from .clustering import partition_cells_by_kmeans
 @timer(logger=logger)
 def run_scrublet(
     data: MultimodalData,
-    expected_doublet_rate: float = 0.1,
-    nPC: int = 30,
-    random_state: int = 0,
-    verbose: bool = True,
+    channel_attr: Optional[str] = None, 
+    expected_doublet_rate: Optional[float] = 0.1,
+    nPC: Optional[int] = 30,
+    output_plot_prefix: Optional[str] = None,
+    random_state: Optional[int] = 0,
+    verbose: Optional[bool] = True,
 ) -> None:
-    """Calculate doublet scores using Scrublet.
+    """Calculate doublet scores using Scrublet for each channel on the current associated data.X matrix.
 
     This is a wrapper of `Scrublet <https://github.com/AllonKleinLab/scrublet>`_ package.
 
@@ -39,8 +41,14 @@ def run_scrublet(
     data: ``MultimodalData`` object.
         Annotated data matrix with rows for cells and columns for genes.
 
+    channel_attr: ``str``, optional, default: None
+        Attribute indicating sample channels. If None, consider all data as one channel.
+
     expected_doublet_rate: ``float``, optional, default: ``0.1``
         The expected doublet rate for the experiment.
+    
+    output_plot_prefix: ``str``, optional, default: None
+        If this option is not None, output Scrublet histogram plots using output_plot_prefix as file name prefix.
 
     nPC: ``int``, optional, default: ``30``
         Number of principal components used to embed the transcriptomes prior to k-nearest-neighbor graph construction.
@@ -56,36 +64,57 @@ def run_scrublet(
     ``None``
 
     Update ``data.obs``:
-        * ``data.obs['scrublet_scores']``: The calculated doublet scores on cells.
+        * ``data.obs['scrublet_score']``: The calculated doublet scores on cells.
 
     Update ``data.uns``:
         * ``data.uns['scrublet_stats']``: Overall stats during the calculation.
 
-    Save doublet histogram as a PDF file named ``scrublet_hist.pdf``.
+    If output_plot_prefix is not None, save doublet histogram as PDF files named ``output_plot_prefix.scrublet.pdf`` or ``output_plot_prefix_{channel}.scrublet.pdf``
 
     Examples
     --------
     >>> pg.run_scrublet(data)
     """
+    def _get_scrublet_info(scrub):
+        scrublet_info = dict()
+        scrublet_info['threshold'] = scrub.threshold_
+        scrublet_info['detected_doublet_rate'] = scrub.detected_doublet_rate_
+        scrublet_info['detectable_doublet_fraction'] = scrub.detectable_doublet_fraction_
+        scrublet_info['overall_doublet_rate'] = scrub.overall_doublet_rate_
+        return scrublet_info
 
     import scrublet as scr
 
-    scrub = scr.Scrublet(data.X, expected_doublet_rate=expected_doublet_rate, random_state=random_state)
-    doublet_scores, predicted_doublets = scrub.scrub_doublets(n_prin_comps=nPC, verbose=verbose)
-    fig, axs = scrub.plot_histogram()
-    fig.savefig("scrublet_hist.pdf")
-    logger.info("Doublet score histogram is saved as 'scrublet_hist.pdf'.")
+    if channel_attr is None:
+        scrub = scr.Scrublet(data.X, expected_doublet_rate=expected_doublet_rate, random_state=random_state)
+        doublet_scores, predicted_doublets = scrub.scrub_doublets(n_prin_comps=nPC, verbose=verbose)
+        if output_plot_prefix is not None:
+            fig, axs = scrub.plot_histogram()
+            fig.savefig(f"{output_plot_prefix}.scrublet.pdf")
+        data.obs['scrublet_score'] = doublet_scores
+        data.uns['scrublet_stats'] = _get_scrublet_info(scrub)
+    else:
+        if not is_categorical_dtype(data.obs[channel_attr]):
+            data.obs[channel_attr] = pd.Categorical(data.obs[channel_attr])
+        scrublet_info_dict = {}
+        scrublet_scores = np.zeros(data.shape[0], dtype = np.float32)
+        for channel in data.obs[channel_attr].cat.categories:
+            idx = (data.obs[channel_attr] == channel).values
+            X_channel = data.X[idx]
+            scrub = scr.Scrublet(X_channel, expected_doublet_rate=expected_doublet_rate, random_state=random_state)
+            doublet_scores, predicted_doublets = scrub.scrub_doublets(n_prin_comps=nPC, verbose=verbose)
+            if output_plot_prefix is not None:
+                fig, axs = scrub.plot_histogram()
+                fig.savefig(f"{output_plot_prefix}_{channel}.scrublet.pdf")
+            scrublet_scores[idx] = doublet_scores
+            scrublet_info_dict[channel] = _get_scrublet_info(scrub)
+            if verbose:
+                logger.info(f"Channel {channel} is processed.")
+        data.obs['scrublet_score'] = scrublet_scores
+        data.uns['scrublet_stats'] = scrublet_info_dict
 
-    data.obs['scrublet_score'] = doublet_scores
-
-    scrublet_info = dict()
-    scrublet_info['threshold'] = scrub.threshold_
-    scrublet_info['detected_doublet_rate'] = scrub.detected_doublet_rate_
-    scrublet_info['detectable_doublet_fraction'] = scrub.detectable_doublet_fraction_
-    scrublet_info['overall_doublet_rate'] = scrub.overall_doublet_rate_
-    data.uns['scrublet_stats'] = scrublet_info
-
-    logger.info("Scrublet is finished.")
+    if verbose:
+        logger.info("Scrublet is finished.")
 
 
 
@@ -100,20 +129,21 @@ def _one_tail_test(scores: List[float], mean: float, std: float, alpha: float = 
     return outliers
 
 
-def _identify_cell_doublets(scores: List[float], alpha: float = 0.05, random_state: int = 0):
+def _identify_cell_doublets(scores: List[float], alpha: float = 0.05, min_dbl_rate: float = 0.01, random_state: int = 0):
     scores = np.log(scores) # log transformed
     scores_reshaped = scores.reshape(-1, 1)
+    min_dbl = scores.size * min_dbl_rate
     # First fit three normal distributions
     gm = GaussianMixture(n_components = 3, random_state = random_state)
     gm.fit(scores_reshaped)
     means = gm.means_.ravel()
     stds = np.sqrt(gm.covariances_.ravel())
     pos = np.argsort(means)[1]
-    outliers = _one_tail_test(scores, means[pos], stds[pos], alpha)
-    prev_ndbl = outliers.sum()
+    prev_outliers = _one_tail_test(scores, means[pos], stds[pos], alpha)
+    prev_ndbl = prev_outliers.sum()
     # Fit two normals by excluding outliers
     gm.set_params(n_components = 2)
-    gm.fit(scores_reshaped[~outliers])
+    gm.fit(scores_reshaped[~prev_outliers])
     means = gm.means_.ravel()
     stds = np.sqrt(gm.covariances_.ravel())
     pos = np.argsort(means)[1]
@@ -121,20 +151,27 @@ def _identify_cell_doublets(scores: List[float], alpha: float = 0.05, random_sta
     ndbl = outliers.sum()
     # Iteratively reduce false cell doublets
     gm.set_params(warm_start = True)
-    while ndbl < prev_ndbl:
+    while ndbl < prev_ndbl and ndbl >= min_dbl:
         gm.fit(scores_reshaped[~outliers])
         means = gm.means_.ravel()
         stds = np.sqrt(gm.covariances_.ravel())
         pos = np.argsort(means)[1]
-        outliers = _one_tail_test(scores, means[pos], stds[pos], alpha)
+        prev_outliers = outliers
         prev_ndbl = ndbl
+        outliers = _one_tail_test(scores, means[pos], stds[pos], alpha)
         ndbl = outliers.sum()
+    
+    if ndbl < min_dbl:
+        # Did not run until converge, roll back
+        outliers = prev_outliers
+        gm.set_params(warm_start = False)
+        gm.fit(scores_reshaped[~outliers])
     # Predict singlets and transition cells
     preds = gm.predict(scores_reshaped[~outliers])
     if pos == 0:
         preds = 1 - preds
     # Generate labels: 0, singlets; 1, transition; 2, doublets
-    labels = np.zeros(scores.size)
+    labels = np.zeros(scores.size, dtype = np.int32)
     labels[outliers] = 2
     labels[~outliers] = preds
 
@@ -146,8 +183,8 @@ def _identify_doublets_fisher(cluster_labels: Union[pd.Categorical, List[int]], 
     df = pd.crosstab(cluster_labels, dbls)
 
     ndbl = df[True].sum()
-    a = df[True].values.astype(np.uint)
-    b = df[False].values.astype(np.uint)
+    a = df[True].values.astype(np.int32)
+    b = df[False].values.astype(np.int32)
     c = ndbl - a
     d = (dbl_codes.size - ndbl) - b
 
@@ -181,6 +218,7 @@ def infer_doublets(
     n_init: Optional[int] = 10,
     min_avg_cells_per_final_cluster: Optional[int] = 10,
     alpha: Optional[float] = 0.05,
+    min_dbl_rate: Optional[float] = 0.01,
     random_state: Optional[int] = 0,
     verbose: Optional[bool] = False,
 ) -> None:
@@ -223,6 +261,9 @@ def infer_doublets(
     alpha: ``float``, optional, default: ``0.05``
         FDR significant level for statistical tests.
 
+    min_dbl_rate: ``float``, optional, default: ``0.01``
+        Minimum expected doublet rate for one channel. In some cases, the algorithm will iterate until no doublet detected, which is contradict with our expectation that at least a small percent of cells are doublets. With this parameter, the algorithm will stop before the detected ratio of doublets is less than ``min_dbl_rate``.
+
     random_state: ``int``, optional, default: ``0``
         Random seed for reproducing results.
 
@@ -255,7 +296,7 @@ def infer_doublets(
             dbl_scores = data.obs.loc[idx, dbl_attr].values
 
             # Cell-level doublet
-            dblc_codes = _identify_cell_doublets(dbl_scores, alpha = alpha, random_state = random_state) # dblc doublet at cell level
+            dblc_codes = _identify_cell_doublets(dbl_scores, alpha = alpha, min_dbl_rate = min_dbl_rate, random_state = random_state) # dblc doublet at cell level
 
             idx_dblc = dblc_codes == 2
             idx_dblnc = ~idx_dblc
