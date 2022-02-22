62d5e0fd92a0286cc35d55ec7f8965e69a0603ff
Rimte Rocher <rimte.rocher@contractors.roche.com>
2022-02-22 17:07:53 -0500

    remove unused imports

--- pegasus/tools/fgsea.py
+++ pegasus/tools/fgsea.py
@@ -1,12 +1,10 @@
-import numpy as np
-import pandas as pd
 import logging
+
 logger = logging.getLogger(__name__)
 
 from pegasus.tools import predefined_pathways, load_signatures_from_file
 from pegasusio import MultimodalData, UnimodalData, timer
-from typing import Union, Optional, List
-
+from typing import Union, Optional
 
 
 @timer(logger=logger)
@@ -33,7 +31,7 @@ def fgsea(
 
     pathways: ``str``
         Either a string or a path to the gene set file in GMT format. If string, choosing from "hallmark" and "canonical_pathways" (MSigDB H and C2/CP).
-    
+
     de_key: ``str``, optional, default: ``"de_res"``
         Key name of DE analysis results stored. data.varm[de_key] should contain a record array of DE results.
 
@@ -70,19 +68,21 @@ def fgsea(
         from rpy2.robjects.conversion import localconverter
     except ModuleNotFoundError as e:
         import sys
+
         logger.error(f"{e}\nNeed rpy2! Try 'pip install rpy2'.")
         sys.exit(-1)
 
     try:
-        fgsea = importr('fgsea')
+        fgsea = importr("fgsea")
     except ModuleNotFoundError:
         import sys
+
         text = """Please install fgsea in order to run this function.\n
                 To install this package, start R and enter:\n
                 if (!require("BiocManager", quietly = TRUE))
                     install.packages("BiocManager")
                 BiocManager::install("fgsea")"""
-                
+
         logger.error(text)
         sys.exit(-1)
 
@@ -92,13 +92,15 @@ def fgsea(
     log2fc = ro.FloatVector(data.varm[de_key][log2fc_key])
     log2fc.names = ro.StrVector(data.var_names)
     res = fgsea.fgsea(pathways_r, log2fc, minSize=minSize, maxSize=maxSize, nproc=nproc)
-    unlist = ro.r('''
+    unlist = ro.r(
+        """
         function(df) {
             df$leadingEdge <- sapply(df$leadingEdge, function(x) {paste(unlist(x), collapse=',')})
             return(df)
         }   
-        ''')
+        """
+    )
     with localconverter(ro.default_converter + pandas2ri.converter):
         res_df = ro.conversion.rpy2py(unlist(res))
-    res_df.sort_values('padj', inplace=True)
+    res_df.sort_values("padj", inplace=True)
     data.uns[fgsea_key] = res_df
