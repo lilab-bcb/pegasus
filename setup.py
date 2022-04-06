dea729c408aba22d59b6545d905c0d321bd9bce0
Tony Kuo <kuot@huzi.gene.com>
2022-04-06 11:09:49 -0700

    update python req to 3.7

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         scvi=["scvi-tools"],
         all=["fitsne", "louvain", "scanorama", "torch", "harmony-pytorch", "nmf-torch", "rpy2", "forceatlas2-python", "scvi-tools"]
     ),
-    python_requires="~=3.6",
+    python_requires="~=3.7",
     package_data={
         "pegasus.annotate_cluster": [
             "human_immune_cell_markers.json",
