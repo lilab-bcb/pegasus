934b868fcb60c93f507f898fd5df95fe7f7f93cd
Yiming Yang <yang.yihming@gmail.com>
2024-03-15 23:46:05 -0700

    Add suport for python 3.12

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="single cell/nucleus genomics analysis",
     packages=find_packages(),
