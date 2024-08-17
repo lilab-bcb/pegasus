1905649a272db61383b965ba9037da31c7ce8205
Yiming Yang <yang.yihming@gmail.com>
2024-08-17 00:57:46 +0000

    update

--- pegasus/tools/empty_drops.py
+++ pegasus/tools/empty_drops.py
@@ -1,4 +1,5 @@
 import itertools
+import numba
 
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ from pegasusio import MultimodalData, UnimodalData
 from pegasus.tools import eff_n_jobs
 from pegasus.tools import SimpleGoodTuring
 from anndata import AnnData
-from scipy.special import loggamma
+from scipy.special import loggamma, factorial
 from scipy.sparse import coo_matrix
 from scipy.interpolate import UnivariateSpline
 from statsmodels.stats.multitest import fdrcorrection as fdr
@@ -67,25 +68,33 @@ def empty_drops(
         alpha = _estimate_alpha(G, ambient_proportion)
         logger.info(f"Calculating alpha is finished. Estimate alpha = {alpha}.")
 
-    t_b = X.sum(axis=1).A1
-    L_b = _logL(alpha, ambient_proportion, t_b, X)
-    n_cells = t_b.size
+    cells_test = df_droplets.loc[df_droplets["n_counts"] > thresh_low].index.values
+    idx_test = df_droplets.index.get_indexer(cells_test)
+    logger.info(f"Run test through {idx_test.size} cells passing thresh_low.")
+
+    T = X[idx_test, :]
+    t_b = T.sum(axis=1).A1
+    L_b = _logL(alpha, ambient_proportion, t_b, T)
 
     n_jobs = eff_n_jobs(n_jobs)
-    n_below = _test_ambient(alpha, ambient_proportion, t_b, L_b, n_iters, random_state, chunk_size, n_jobs)
-    logger.info("Calculation on p-values is finished.")
+    n_below = _test_empty_drops(alpha, ambient_proportion, t_b, L_b, n_iters, random_state, n_jobs)
     pval = (n_below + 1) / (n_iters + 1)
-    _, qval = fdr(pval)
-
-    data.obs["EmptyDrops.pval"] = pval
-    data.obs["EmptyDrops.qval"] = qval
+    logger.info("Calculation on p-values is finished.")
 
     df_rank_stats, knee, inflection = _rank_barcode(t_b, thresh_low, exclude_from)
-    logger.info("Calculate the knee point.")
+    idx_always = np.where(t_b >= knee)[0]
+    pval[idx_always] = 0.0
+    logger.info("Adjust p-values by the estimated knee point.")
 
-    data.obs["EmptyDrops.quality"] = "low"
-    data.obs.loc[data.obs["EmptyDrops.qval"]<significance_level, "EmptyDrops.quality"] = "high"
-    data.obs.loc[data.obs["n_counts"]>inflection, "EmptyDrops.quality"] = "high"
+    _, qval = fdr(pval)
+    logger.info
+
+    data.obs["EmptyDrops.pval"] = np.nan
+    data.obs.loc[cells_test, "EmptyDrops.pval"] = pval
+    data.obs["EmptyDrops.qval"] = np.nan
+    data.obs.loc[cells_test, "EmptyDrops.qval"] = qval
+
+    return (df_rank_stats, knee, inflection)
 
 
 def _rank_barcode(total_counts, thresh_low, exclude_from):
@@ -117,7 +126,7 @@ def _rank_barcode(total_counts, thresh_low, exclude_from):
         return [element for element, count in zip(vals, lens) for _ in range(count)]
 
     df_stats = pd.DataFrame({
-        "Barcodes": repeat(tie_rank, rank_sizes),
+        "Barcodes": repeat(np.log(tie_rank), rank_sizes),
         "n_counts": repeat(rank_values, rank_sizes),
         "UMI counts": repeat(fitted_values, rank_sizes),
     })
@@ -136,50 +145,117 @@ def _find_curve_bounds(x, y, exclude_from):
     return (left_edge + skip, right_edge + skip)
 
 
-def _test_ambient(alpha, prop, t_b, L_b, n_iters, random_state, chunk_size, n_jobs):
-    n_cells = t_b.size
-    n_genes = prop.size
-    n_below = np.zeros(n_cells, dtype=int)
-    np.random.seed(random_state)
+def _test_empty_drops(alpha, prop, t_b, L_b, n_iters, random_state, n_jobs):
+    tb_dict = {}
+    for idx, t in enumerate(t_b):
+        if t in tb_dict:
+            tb_dict[t].append(idx)
+        else:
+            tb_dict[t] = [idx]
+    tb_unique = list(tb_dict.keys())
+    tb_unique.sort()
 
-    p_arr = np.random.dirichlet(alpha * prop, size=n_iters)
-    print(p_arr.shape)
+    np.random.seed(random_state)
+    p_array = np.random.dirichlet(alpha * prop, size=n_iters)
+    chunk_size = n_iters // n_jobs
     chunks = process_map(
-        _test_ambient_by_chunk,
+        _test_empty_drops_by_chunk,
         itertools.repeat(alpha),
         itertools.repeat(prop),
-        itertools.repeat(p_arr),
-        [t_b[i:(i+chunk_size)] for i in range(0, n_cells, chunk_size)],
-        [L_b[i:(i+chunk_size)] for i in range(0, n_cells, chunk_size)],
+        itertools.repeat(tb_unique),
+        itertools.repeat(tb_dict),
+        itertools.repeat(t_b),
+        itertools.repeat(L_b),
+        itertools.repeat(random_state),
+        [p_array[i:(i+chunk_size)] for i in range(0, n_iters, chunk_size)],
         tqdm_class=tqdm,
         max_workers=n_jobs,
     )
-    return np.hstack(chunks)
+    return np.vstack(chunks).sum(axis=0)
 
 
-def _test_ambient_by_chunk(alpha, prop, p_arr, t_b, L_b):
-    n_below = np.zeros(t_b.size, dtype=int)
+def _test_empty_drops_by_chunk(alpha, prop, tb_unique, tb_dict, t_b, L_b, random_state, p_arr):
+    tb_min = tb_unique[0]
+    idx_all_genes = np.arange(prop.size)
+    n_below = np.zeros(t_b.size)
+    L_test = np.zeros(t_b.size)
+
+    np.random.seed(random_state)
     for p in p_arr:
-        samples = coo_matrix([np.random.multinomial(t, p) for t in t_b])
-        L_test = _logL(alpha, prop, t_b, samples)
+        for t in tb_unique:
+            if t == tb_min:
+                z = np.random.multinomial(t, p)
+                L = _logL(alpha, prop, np.array(t), coo_matrix(z))
+            else:
+                idx_extra_genes = np.random.choice(idx_all_genes, size=t-t1, replace=True, p=p)
+                for i in idx_extra_genes:
+                    z[i] += 1
+                L = _calc_logL_by_increment(L1, alpha, prop, t, t - t1, z, idx_extra_genes)
+
+            L_test[tb_dict[t]] = L
+            t1 = t
+            L1 = L
         n_below += (L_test <= L_b)
+
     return n_below
 
 
+def _calc_logL_by_increment(L1, alpha, prop, t, n_extra_counts, z, idx_extra_genes):
+    return (
+        L1
+        + n_extra_counts * np.log(t / (t + alpha - 1))
+        + np.sum(np.log((z[idx_extra_genes] + alpha * prop[idx_extra_genes] - 1) / z[idx_extra_genes]))
+    )
+
+
+#def _test_empty_drops(alpha, prop, t_b, L_b, n_iters, random_state, chunk_size, n_jobs):
+#    n_cells = t_b.size
+#    n_genes = prop.size
+#    n_below = np.zeros(n_cells, dtype=int)
+#    np.random.seed(random_state)
+#
+#    #p = np.random.dirichlet(alpha * prop, size=n_iters)
+#    n_below = np.zeros(n_cells, dtype=int)
+#    for k in range(n_iters):
+#        p = np.random.dirichlet(alpha * prop)
+#        chunks = process_map(
+#            _test_ambient_by_chunk,
+#            itertools.repeat(alpha),
+#            itertools.repeat(prop),
+#            itertools.repeat(p),
+#            [t_b[i:(i+chunk_size)] for i in range(0, n_cells, chunk_size)],
+#            [L_b[i:(i+chunk_size)] for i in range(0, n_cells, chunk_size)],
+#            tqdm_class=tqdm,
+#            max_workers=n_jobs,
+#        )
+#        n_below += np.hstack(chunks)
+#        if (k + 1) % 1000 == 0:
+#            print(f"{k+1} iterations finished.")
+#    return n_below
+
+
+#def _test_ambient_by_chunk(alpha, prop, p, t_b, L_b):
+#    n_below = np.zeros(t_b.size, dtype=int)
+#    samples = coo_matrix([np.random.multinomial(t, p) for t in t_b])
+#    L_test = _logL(alpha, prop, t_b, samples)
+#    return (L_test <= L_b)
+
+
 def _estimate_alpha(G, prop):
-    total_counts = G.sum(axis=0).A1
+    total_counts = G.sum(axis=1).A1
     n_cells = total_counts.size
     indices = G.nonzero()[1]
     x = G.data
 
     def neg_logL(alpha):
+        # TODO: Maybe remove terms not related to alpha
         return -(
             np.sum(loggamma(total_counts + 1))
             + n_cells * loggamma(alpha)
             - np.sum(loggamma(total_counts + alpha))
             + np.sum(loggamma(x + alpha * prop[indices]))
             - np.sum(loggamma(x + 1))
-            - np.sum(n_cells * loggamma(alpha * prop))
+            - np.sum(n_cells * loggamma(alpha * prop[indices]))
         )
 
     estimator = so.minimize(neg_logL, np.array([1]), bounds=so.Bounds(0.01, 10000))
@@ -192,13 +268,14 @@ def _logL(alpha, prop, total_counts, Y):
     idx_cells, idx_genes = Y.nonzero()
     x = Y.data
     Y_ag = coo_matrix((loggamma(x + alpha * prop[idx_genes]), (idx_cells, idx_genes)), shape=(n_cells, n_genes))
-    Y_p1 = coo_matrix((loggamma(x + 1), (idx_cells, idx_genes)), shape=(n_cells, n_genes))
+    Y_fact = coo_matrix((loggamma(x + 1), (idx_cells, idx_genes)), shape=(n_cells, n_genes))
+    A_g = coo_matrix((loggamma(alpha * prop[idx_genes]), (idx_cells, idx_genes)), shape=(n_cells, n_genes))
 
     return (
         loggamma(total_counts + 1)
         + loggamma(alpha)
         - loggamma(total_counts + alpha)
         + np.asarray(Y_ag.sum(axis=1)).squeeze()
-        - np.asarray(Y_p1.sum(axis=1)).squeeze()
-        - np.sum(loggamma(alpha * prop))
+        - np.asarray(Y_fact.sum(axis=1)).squeeze()
+        - np.asarray(A_g.sum(axis=1)).squeeze()
     )
