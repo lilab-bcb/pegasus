458f434d08ce43dc267ee9c7a8218cbcd7b460d3
Yiming Yang <yang.yihming@gmail.com>
2025-05-22 19:34:42 -0700

    Doublet detection improvement (#318)
    * Incorporate new code
    
    * Updated
    
    * Adjusted some parameters
    
    * clean whitespaces
    
    * resolve warnings
    
    * drop support on py38; add support for py313
    
    * enforce numpy<2 for py39
    
    * remove tsne from CI test
    
    ---------
    
    Co-authored-by: Bo Li <li.bo@gene.com>

--- tests/run_pipeline.sh
+++ tests/run_pipeline.sh
@@ -1,7 +1,7 @@
 pegasus aggregate_matrix tests/data/count_matrix.csv tests/aggr
 
 if [ -f "tests/aggr.zarr.zip" ]; then
-    pegasus cluster -p 2 --min-genes 500 --max-genes 6000 --percent-mito 20.0 --output-filtration-results --output-h5ad --output-loom --plot-filtration-results --plot-hvf --exact-K --correct-batch-effect --nmf --leiden --tsne --umap --fle --infer-doublets --dbl-cluster-attr leiden_labels --calc-signature-scores cell_cycle_mouse tests/aggr.zarr.zip tests/result
+    pegasus cluster -p 2 --min-genes 500 --max-genes 6000 --mito-prefix mt- --percent-mito 20.0 --output-filtration-results --output-h5ad --output-loom --plot-filtration-results --plot-hvf --exact-K --correct-batch-effect --nmf --leiden --umap --fle --calc-signature-scores cell_cycle_mouse tests/aggr.zarr.zip tests/result
 fi
 
 if [ -f "tests/result.zarr.zip" ]; then
@@ -9,6 +9,5 @@ if [ -f "tests/result.zarr.zip" ]; then
     pegasus annotate_cluster --markers mouse_immune,mouse_brain tests/result.zarr.zip tests/result.anno.txt
     pegasus plot compo --groupby leiden_labels --condition Channel tests/result.zarr.zip tests/result.compo.pdf
     pegasus plot scatter --basis umap --attributes leiden_labels,Channel tests/result.zarr.zip tests/result.leiden_labels.umap.pdf
-    pegasus plot scatter --basis tsne --attributes leiden_labels,Channel tests/result.zarr.zip tests/result.leiden_labels.tsne.pdf
     pegasus plot scatter --basis fle --attributes leiden_labels,Channel tests/result.zarr.zip tests/result.leiden_labels.fle.pdf
 fi
