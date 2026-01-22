06b1df1c8fedf424395c693bc21bbc61b44a88d8
Yiming Yang <yang.yihming@gmail.com>
2026-01-21 17:52:14 -0800

    Deprecate pkg_resources (#324)
    * Use importlib.resources to replace pkg_resources
    
    * drop CI test for py3.9, add py3.14
    
    * remove louvain
    
    * fix typos

--- tests/run_inmf.sh
+++ tests/run_inmf.sh
@@ -1,5 +1,5 @@
 pegasus aggregate_matrix tests/data/count_matrix.csv tests/aggr
 
 if [ -f "tests/aggr.zarr.zip" ]; then
-    pegasus cluster -p 2 --output-h5ad --output-loom --correct-batch-effect --correction-method inmf --louvain --umap tests/aggr.zarr.zip tests/inmf_result
+    pegasus cluster -p 2 --output-h5ad --output-loom --correct-batch-effect --correction-method inmf --leiden --umap tests/aggr.zarr.zip tests/inmf_result
 fi
